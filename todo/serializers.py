206b413cc824d47718f8b78b336ad5efee0776b8
Manan Jain <mananjain@Manans-MacBook-Air.local>
2021-09-05 12:14:23 +0530

    models

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,29 +1,14 @@
 # use of serializer is to convert my table data into Json data
+from typing import List
 from django.contrib.auth.models import User, Group
 from django.db.models.fields.related import ManyToManyField
-from rest_framework import serializers 
-from  .models import  card, project ,User
+from rest_framework import serializers
+from todo import models 
+from  .models import  card, project ,User , list
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
 
-
-# class projectserializer(serializers.Serializer):
-
-#     projtitle = serializers.CharField(max_length=50)
-#     wiki = serializers.CharField(max_length=500)
-#     member = User.objects.get(projtitle = projtitle.text).member
-#     def create(self, validated_data):
-#         user = project.objects.create(
-#             projtitle=validated_data['projtitle'],
-#             wiki=validated_data['wiki'],
-#             member = validated_data['member'],
-                     
-#         )
-#         return user
-#     class Meta:
-#         model = project
-#         fields = ["projtitle","wiki","member"]
 class projectserializer(serializers.ModelSerializer):
     class Meta:
         model = project
--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,8 @@ from todo import views
 urlpatterns = [
     path('channeli',views.student_detail),
     path('login',views.student),
-    path('get',views.main_page),
-    path('in',views.login2),
     path('project',views.project.as_view()),
+    path('project/<int:project_id>',views.list.as_view()),
+    path('project/<int:project_id>/<int:list_id>',views.card.as_view()),
+    
 ]
